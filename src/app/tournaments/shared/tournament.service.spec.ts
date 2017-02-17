73c00e7e92712cc34bbe22d50d22bc1a86e711d3
Tomasz Morek <t.morek@gmail.com>
2017-02-17 21:12:02 +0100

    User real server for tournaments.service

--- src/app/tournaments/shared/tournament.service.spec.ts
+++ src/app/tournaments/shared/tournament.service.spec.ts
@@ -1,11 +1,14 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { Http } from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { TournamentService } from './tournament.service';
 
 describe('TournamentService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpModule],
       providers: [TournamentService]
     });
   });
--- src/app/tournaments/shared/tournament.service.ts
+++ src/app/tournaments/shared/tournament.service.ts
@@ -1,20 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
 import { Tournament } from './tournament.model';
 import { TOURNAMENTS } from '../../testing/tournament-stubs';
+import { environment } from '../../../environments/environment';
+import 'rxjs/add/operator/toPromise';
+
 
 @Injectable()
 export class TournamentService {
 
-  constructor() { }
+  private tournamentsUrl = environment.serverUrl + '/tournaments';
 
-  getTournament(id: string): Promise<Tournament> {
-    return Promise.resolve(TOURNAMENTS.find(tournament => tournament.id === id));
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor(private http: Http) { }
+
+  getTournament(id: string) {
+    let url = `${this.tournamentsUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json() as Tournament)
+      .catch(this.handleError);
   }
 
   create(tournament: Tournament) {
-    let lastId = TOURNAMENTS[TOURNAMENTS.length - 1].id;
-    tournament.id = String(+lastId + 1);
-    TOURNAMENTS.push(tournament);
-    return Promise.resolve(tournament);
+    let url = this.tournamentsUrl;
+    return this.http.post(url, JSON.stringify(tournament), {headers: this.headers})
+      .toPromise()
+      .then(response => response.json() as Tournament)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occured', error);
+    return Promise.reject(error.message || error);
   }
 }
