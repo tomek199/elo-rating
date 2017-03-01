8848997c73e09ec7a72be43d2f49460b68df5b8d
Tomasz Morek <t.morek@gmail.com>
2017-03-01 20:38:20 +0100

    Upgrade dependency to Bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { RoutingModule } from './core/routing/routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { PlayerAddComponent } from './players/player-add/player-add.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    RoutingModule
+    RoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [TournamentService, PlayerService],
   bootstrap: [AppComponent]
