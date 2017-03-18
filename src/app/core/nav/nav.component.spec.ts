6f36dcc2db0f8396622bb681d7ead8e10f55e3fa
Tomasz Morek <t.morek@gmail.com>
2017-03-18 20:43:48 +0100

    Add url to component match-add in navbar

--- src/app/core/nav/nav.component.spec.ts
+++ src/app/core/nav/nav.component.spec.ts
@@ -58,7 +58,7 @@ describe('NavComponent', () => {
   it ('should render navbar for user which selected league', () => {
     let debugElement = fixture.debugElement.queryAll(By.css('nav ul.navbar-nav li a'));
     expect(debugElement[0].nativeElement.textContent).toEqual('Dashboard');
-    expect(debugElement[1].nativeElement.textContent).toEqual('Rating');    
+    expect(debugElement[1].nativeElement.textContent).toEqual('Matches');    
     expect(debugElement[2].nativeElement.textContent).toEqual('Players');
     expect(debugElement[3].nativeElement.textContent).toEqual('Queue');
   });
