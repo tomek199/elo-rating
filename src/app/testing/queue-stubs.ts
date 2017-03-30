2457f6f05d6583d013034e3f6f76592692b5cd6e
tpokora <pokora.tomek@gmail.com>
2017-03-30 21:10:38 +0200

    Fix navbar tests

--- src/app/testing/queue-stubs.ts
+++ src/app/testing/queue-stubs.ts
@@ -3,7 +3,7 @@ import { Queue } from './../queue/shared/queue.model';
 import { Injectable } from '@angular/core';
 
 export const QUEUE: Queue = {
-    id: '0',
+    id: '123',
     name: 'testQueue',
     description: 'Test Queue',
     matches: [
