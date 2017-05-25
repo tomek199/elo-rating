c305eedf76c8bd9411ffeaa1ce67b0274f7e6f7f
tpokora <pokora.tomek@gmail.com>
2017-05-25 22:12:33 +0200

    Change queue header to 'Daily Queue'

--- src/app/queue/queue-list/queue-list.component.spec.ts
+++ src/app/queue/queue-list/queue-list.component.spec.ts
@@ -63,7 +63,7 @@ describe('QueueComponent', () => {
     fixture.detectChanges();
     expect(component).toBeTruthy();
     let debugElement = fixture.debugElement.query(By.css('div#daily-queue h2'));
-    expect(debugElement.nativeElement.textContent).toEqual('testQueue');
+    expect(debugElement.nativeElement.textContent).toEqual('Daily Queue');
   });
 
   it('should add match to queue', fakeAsync(() => {
