6f702db02a88c5a76b6d1b962d5a9384fc6ae04a
Szymon Depta <szymon.depta@gmail.com>
2017-05-31 23:09:51 +0200

    Added sets statistics to player's page. Refactored player-statistics template for easier addition of new stats. Resolves #57

--- src/main/elo-rating/src/app/core/utils/charts/matches-stats-chart.ts
+++ src/main/elo-rating/src/app/core/utils/charts/matches-stats-chart.ts
@@ -13,15 +13,27 @@ export class MatchesStatsChart extends ChartBuilder {
     let matchesCount = this.matches.length
     let win = 0;
     let loss = 0;
+    let setsWon = 0;
+    let setsLost = 0;
     this.matches.forEach(match => {
       if (this.isWinner(match)) 
         win += 1;
       else
         loss +=1;
+
+      Object.keys(match.scores).forEach(key => {
+        let value = match.scores[key];
+        if (key == this.playerId)
+          setsWon += value;
+        else
+          setsLost += value;
+      });
     });
-    this.chart.series.push({name: 'Won matches', data: {value: win, max: matchesCount}});
-    this.chart.series.push({name: 'Lost matches', data: {value: loss, max: matchesCount}});
-    this.chart.series.push({name: 'Percentage of winnings', data: {value: this.getPercentage(win), max: 100}})
+    this.chart.series.push({name: 'Won matches', type: 'success', data: {value: win, max: matchesCount}});
+    this.chart.series.push({name: 'Lost matches', type: 'danger', data: {value: loss, max: matchesCount}});
+    this.chart.series.push({name: 'Percentage of winnings', type: 'info', data: {value: this.getPercentage(win), max: 100}, showValue: 'true'});
+    this.chart.series.push({name: 'Sets won', type: 'success', data: {value: setsWon, max: setsWon + setsLost}});
+    this.chart.series.push({name: 'Sets lost', type: 'danger', data: {value: setsLost, max: setsWon + setsLost}});
   }
 
   private isWinner(match: Match): boolean {
