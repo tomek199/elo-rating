aaf97853cc9fd3eeb6b334d813327b41a2435977
Tomasz Morek <t.morek@gmail.com>
2017-07-25 14:34:24 +0200

    Change small-spinner selector

--- src/main/elo-rating/src/app/core/directives/small-spinner/small-spinner.component.ts
+++ src/main/elo-rating/src/app/core/directives/small-spinner/small-spinner.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-spinner',
+  selector: 'app-small-spinner',
   templateUrl: './small-spinner.component.html',
   styleUrls: ['./small-spinner.component.css']
 })
