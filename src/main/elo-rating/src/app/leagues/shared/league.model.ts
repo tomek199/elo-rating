e570367545eb82d1c13ace0bd054b472bd9386ec
Tomasz Morek <t.morek@gmail.com>
2017-08-08 13:50:09 +0200

    Update League model by users array

--- src/main/elo-rating/src/app/leagues/shared/league.model.ts
+++ src/main/elo-rating/src/app/leagues/shared/league.model.ts
@@ -1,6 +1,8 @@
+import { User } from './../../users/shared/user.model';
 export class League {
   id: string;
   name: string;
+  users: User[];
 
   constructor(id?: string, name?: string) {
     this.id = id;
--- src/main/elo-rating/src/app/testing/data/leagues.ts
+++ src/main/elo-rating/src/app/testing/data/leagues.ts
@@ -1,7 +1,7 @@
 import { League } from './../../leagues/shared/league.model';
 
 export const LEAGUES: League[] = [
-  {id: '123', name: 'League name'},
-  {id: '222', name: 'League two'},
-  {id: '333', name: 'League three'}
+  {id: '123', name: 'League name', users: null},
+  {id: '222', name: 'League two', users: null},
+  {id: '333', name: 'League three', users: null}
 ]
