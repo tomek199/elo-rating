bd11d03cb4c6c3f909f0308b7ca634ad9083c332
Tomasz Morek <t.morek@gmail.com>
2017-09-16 13:13:22 +0200

    Update environments by new parameter

--- src/main/elo-rating/src/environments/environment.prod.ts
+++ src/main/elo-rating/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true, 
   serverUrl: 'http://elo-rating-core.herokuapp.com',
-  googleClientId: '146308360723-unuhgufmab9e4knd6ibupei68t18grni.apps.googleusercontent.com'
+    //serverUrl: 'http://localhost:8080',
+  googleClientId: '146308360723-unuhgufmab9e4knd6ibupei68t18grni.apps.googleusercontent.com',
+  matchesRefreshTime: 5000  
 };
--- src/main/elo-rating/src/environments/environment.ts
+++ src/main/elo-rating/src/environments/environment.ts
@@ -6,6 +6,7 @@
 export const environment = {
   production: false,
   serverUrl: 'http://elo-rating-core.herokuapp.com',
-  googleClientId: '146308360723-unuhgufmab9e4knd6ibupei68t18grni.apps.googleusercontent.com'
   //serverUrl: 'http://localhost:8080',
+  googleClientId: '146308360723-unuhgufmab9e4knd6ibupei68t18grni.apps.googleusercontent.com',
+  matchesRefreshTime: 5000
 };
