81a9e7388e521fc05375e250e05a4d529b76f220
Tomasz Morek <t.morek@gmail.com>
2017-10-07 16:55:04 +0200

    Change server url to https

--- elo-rating-frontend/src/environments/environment.prod.ts
+++ elo-rating-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: 'http://elo-rating.herokuapp.com',
+  serverUrl: 'https://elo-rating.herokuapp.com',
   //serverUrl: 'http://localhost:8080',
   googleClientId: '146308360723-unuhgufmab9e4knd6ibupei68t18grni.apps.googleusercontent.com',
   matchesRefreshTime: 5000,
