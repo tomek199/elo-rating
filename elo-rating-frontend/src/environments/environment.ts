78f3422bc8f893f8ed7e8cb02cbeb40bd3fc57ff
Tomasz Morek <t.morek@gmail.com>
2017-10-07 16:18:31 +0200

    Merge two projects version two

--- elo-rating-frontend/src/environments/environment.ts
+++ elo-rating-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  serverUrl: 'http://elo-rating-core.herokuapp.com',
+  serverUrl: 'http://elo-rating.herokuapp.com',
   //serverUrl: 'http://localhost:8080',
   googleClientId: '146308360723-unuhgufmab9e4knd6ibupei68t18grni.apps.googleusercontent.com',
   matchesRefreshTime: 5000,
