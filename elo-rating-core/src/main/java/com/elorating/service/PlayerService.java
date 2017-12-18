ad1d98a3e89950209ae43c3d67bdd732d61566db
Tomasz Morek <t.morek@gmail.com>
2017-12-18 22:30:41 +0100

    Craete PlayerService

---
+++ elo-rating-core/src/main/java/com/elorating/service/PlayerService.java
@@ -0,0 +1,14 @@
+package com.elorating.service;
+
+import com.elorating.model.Player;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public interface PlayerService extends GenericService<Player> {
+
+    List<Player> findByLeagueId(String id);
+    List<Player> getRanking(String id, Sort sort);
+    List<Player> findByLeagueIdAndUsernameLikeIgnoreCase(String leagueId, String username);
+
+}
