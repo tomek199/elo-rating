0dc9fec730ff448f41dfcbe2e0f430d76ba096c1
tpokora <pokora.tomek@gmail.com>
2017-12-26 17:57:25 +0100

    Edited match class

---
+++ elo-rating-core/src/main/java/com/elorating/service/email/EditMatchEmail.java
@@ -0,0 +1,45 @@
+package com.elorating.service.email;
+
+import com.elorating.model.League;
+import org.thymeleaf.context.Context;
+
+public class EditMatchEmail extends EmailBuilder {
+
+    private String opponent;
+    private String recipient;
+    private String matchtime;
+    private String originUrl;
+    private League league;
+
+    public EditMatchEmail(String opponent, String recipient, String matchtime, String originUrl, League league) {
+        this.opponent = opponent;
+        this.recipient = recipient;
+        this.matchtime = matchtime;
+        this.originUrl = originUrl;
+        this.league = league;
+    }
+
+    @Override
+    public void buildRecipient() {
+        email.setRecipient(recipient);
+    }
+
+    @Override
+    public void buildSubject() {
+        email.setSubject(EmailStrings.EDITED_MATCH);
+    }
+
+    @Override
+    public void buildTemplateName() {
+        email.setTemplateName(EmailStrings.CANCELLED_MATCH_TEMPLATE);
+    }
+
+    @Override
+    public void buildContext() {
+        Context context = email.getContext();
+        String redirectUrl = originUrl + "/leagues/" + this.league.getId();
+        context.setVariable("redirectUrl", redirectUrl);
+        context.setVariable("opponent", opponent);
+        context.setVariable("matchtime", matchtime);
+    }
+}
--- elo-rating-core/src/main/java/com/elorating/service/email/EmailStrings.java
+++ elo-rating-core/src/main/java/com/elorating/service/email/EmailStrings.java
@@ -7,11 +7,12 @@ public class EmailStrings {
      */
     public static final String SCHEDULED_MATCH = "EloRating match scheduled!";
     public static final String CANCELLED_MATCH = "EloRating match cancelled!";
+    public static final String EDITED_MATCH = "EloRating match edited!";
 
     /*
         Email templates name
      */
     public static final String SCHEDULED_MATCH_TEMPLATE = "scheduledMatch";
     public static final String CANCELLED_MATCH_TEMPLATE = "cancelledMatch";
-
+    public static final String EDITED_MATCH_TEMPLATE = "editMatch";
 }
