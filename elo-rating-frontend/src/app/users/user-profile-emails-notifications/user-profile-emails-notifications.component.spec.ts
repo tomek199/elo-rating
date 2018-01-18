90f22773d7ba634dae7d7560a55eb1a81029e91a
tpokora <pokora.tomek@gmail.com>
2018-01-18 21:25:36 +0100

    EmailsNotifications testing

--- elo-rating-frontend/src/app/users/user-profile-emails-notifications/user-profile-emails-notifications.component.spec.ts
+++ elo-rating-frontend/src/app/users/user-profile-emails-notifications/user-profile-emails-notifications.component.spec.ts
@@ -1,14 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { UiSwitchModule } from 'angular2-ui-switch';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { UserProfileEmailsNotificationsComponent } from './user-profile-emails-notifications.component';
+import { USERS } from 'app/testing/data/users';
+import { UiSwitcherStub } from 'app/testing/switcher-stubs';
 
-describe('UserProfileEmailsNotificationsComponent', () => {
+fdescribe('UserProfileEmailsNotificationsComponent', () => {
   let component: UserProfileEmailsNotificationsComponent;
   let fixture: ComponentFixture<UserProfileEmailsNotificationsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserProfileEmailsNotificationsComponent ]
+      declarations: [
+        UserProfileEmailsNotificationsComponent,
+        UiSwitcherStub
+       ]
     })
     .compileComponents();
   }));
@@ -16,10 +23,28 @@ describe('UserProfileEmailsNotificationsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserProfileEmailsNotificationsComponent);
     component = fixture.componentInstance;
+    component.user = USERS[0];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should change when clicked', fakeAsync(() => {
+    let originalScheduledMatchNotification = component.user.emailsNotifications.scheduledMatchNotification;
+    let originalEditedMatchNotification = component.user.emailsNotifications.editedMatchNotification;
+    let originalCancelledMatchNotification = component.user.emailsNotifications.cancelledMatchNotification;
+    expect(originalScheduledMatchNotification).toBeFalsy();
+    expect(originalEditedMatchNotification).toBeFalsy();
+    expect(originalCancelledMatchNotification).toBeFalsy();
+
+    let scheduledMatchNotificationSwitcher = fixture.debugElement.queryAll(By.css('ul li span ui-switch'))[0];
+    let editedMatchNotificationSwitcher = fixture.debugElement.queryAll(By.css('ul li span ui-switch'))[1];
+    let cancelledMatchNotificationSwitcher = fixture.debugElement.queryAll(By.css('ul li span ui-switch'))[2];
+    expect(scheduledMatchNotificationSwitcher).toBeTruthy();
+    expect(editedMatchNotificationSwitcher).toBeTruthy();
+    expect(cancelledMatchNotificationSwitcher).toBeTruthy();
+
+  }))
 });
