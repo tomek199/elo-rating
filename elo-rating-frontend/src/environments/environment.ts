30aa982a981c711f7ee2ee9beb1af326262bf900
tpokora <pokora.tomek@gmail.com>
2018-02-18 14:46:57 +0100

    #119 - PROD profile for elo-rating-frontend

--- elo-rating-frontend/src/environments/environment.ts
+++ elo-rating-frontend/src/environments/environment.ts
@@ -5,8 +5,7 @@
 
 export const environment = {
   production: false,
-  serverUrl: 'https://elo-rating.herokuapp.com/api',
-  //serverUrl: 'http://localhost:8080/api',
+  serverUrl: 'http://localhost:8080/api',
   googleClientId: '146308360723-unuhgufmab9e4knd6ibupei68t18grni.apps.googleusercontent.com',
   matchesRefreshing: false,
   matchesRefreshTime: 5000,
