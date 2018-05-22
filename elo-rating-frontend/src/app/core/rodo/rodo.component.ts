a6508af936ab441e000ff01ff50ec796182ae799
tpokora <pokora.tomek@gmail.com>
2018-05-22 20:00:56 +0200

    #134 - Toggle rodo component

---
+++ elo-rating-frontend/src/app/core/rodo/rodo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rodo',
+  templateUrl: './rodo.component.html',
+  styleUrls: ['./rodo.component.css']
+})
+export class RodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
