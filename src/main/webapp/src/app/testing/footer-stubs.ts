900875c89d4054bb3e904322cd8db1e141adbddc
Tomasz Morek <t.morek@gmail.com>
2019-05-12 22:02:21 +0200

    Add footer

---
+++ src/main/webapp/src/app/testing/footer-stubs.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    selector: 'app-footer',
+    template: ''
+})
+export class FooterComponentStub implements OnInit {
+
+    constructor() { }
+    ngOnInit() { }
+}
